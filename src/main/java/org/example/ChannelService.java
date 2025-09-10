a370584aefcf2f7ebb5371c12d19c209e363fba1
AbdelRahmanAlTamimi <tamimi6030@gmail.com>
2025-09-10 23:24:55 +0300

    first try

--- src/main/java/org/example/ChannelService.java
+++ src/main/java/org/example/ChannelService.java
@@ -2,18 +2,54 @@ package org.example;
 
 import org.example.subject.Channel;
 import org.example.subject.Subject;
+import org.example.observer.Observer;
+import java.util.*;
 
 public class ChannelService {
+    private final Map<String, Channel> channels = new HashMap<>();
 
-    public void createNewChannel(Channel channel) {
+    public boolean createChannel(String name) {
         // TODO: implement
+        if (name == null || channels.containsKey(name)) {
+            return false;
+        }
+
+        Channel channel = new Channel(name);
+        channels.put(name,channel);
+        return true;
+    }
+    public boolean deleteChannel(String name) {
+        if (!channels.containsKey(name)) {
+            return false;
+        }
+        channels.remove(name);
+        return true;
+    }
+    public Optional<Channel> getChannel(String name) {
+        return Optional.ofNullable(channels.get(name));
     }
 
-    public void subscribeToChannel(Channel channel, Subject subject) {
-        // TODO: implement
+    public boolean subscribe(String channelName, Observer user) {
+        HashSet<Observer> usersOfChannel = channels.get(channelName).getSubscribers();
+        if (channelName.isBlank() || !this.channels.containsKey(channelName) || usersOfChannel.isEmpty()){
+            return false;
+        }
+        Channel channel = channels.get(channelName);
+        channel.subscribe(user);
+        return true;
     }
 
-    public void newContentNotification(Channel channel, String message) {
-        // TODO: implement
+    public boolean unsubscribe(String channelName, Observer user) {
+        if (channels.get(channelName).getSubscribers().contains(user)) {
+            channels.get(channelName).unsubscribe(user);
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean upload(String channelName, String videoTitle){
+        channels.get(channelName).uploadVideo(videoTitle);
+        return true;
     }
 }
